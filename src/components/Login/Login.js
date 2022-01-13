bcbf1a0e84eea194734201ab1d0ab874c60335a3
RadtkeN <raddnc@gmail.com>
2022-01-12 23:13:18 -0600

    violating use state rule by calling state inside state. going to use reducer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -18,25 +18,31 @@ const Login = (props) => {
     }
   }, [enteredPassword]);
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('checking form validity!')
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500); //lower this number to speed up clean up
-    return () => {
-      console.log('cleanup')
-      clearTimeout(identifier);
-    };
-  }, [enteredEmail, enteredPassword]);
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('checking form validity!')
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500); //lower this number to speed up clean up
+  //   return () => {
+  //     console.log('cleanup')
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(
+      enteredEmail.includes('@') && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
